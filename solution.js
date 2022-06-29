411a60241447f150be76864efd1601250eb9ea43
Danica Yener <ny21037@googlemail.com>
2022-06-29 13:31:26 +0200

    first exercise

--- solution.js
+++ solution.js
@@ -1,1 +1,17 @@
 // write your code in this file
+
+console.log('This is an exercise in console logging');
+
+let exercise = "This is the value.";
+
+console.log("exercise");
+//Name
+
+
+let firstName = 'Danica';
+let lastName = 'Yener';
+let age = 28;
+let job = 'web dev';
+let city = 'Hamburg'
+console.log (firstName, lastName, 'is a ', age,'years old', job, 'who lives in', city )
+// console.log('My Name is Danica, Im 28 years old I live in Hamburg');
